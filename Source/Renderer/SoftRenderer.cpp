2c81499e8f2058dd63a0fe8a4ded2fd6a4b9429a
BBundigi <dl120600@gmail.com>
2020-08-06 13:55:40 +0900

    no message

--- Source/Renderer/SoftRenderer.cpp
+++ Source/Renderer/SoftRenderer.cpp
@@ -1,7 +1,5 @@
-
 #include "SoftRenderer.h"
 #include "Windows/GDIHelper.h"
-
 
 SoftRenderer::SoftRenderer()
 {
@@ -37,6 +35,120 @@ void SoftRenderer::DrawPixel(i32 x, i32 y)
 	*(dest + offset) = mGDIHelper->GetCurrentColor();
 }
 
+void SoftRenderer::DrawTri(float x0, float y0, float x1, float y1, float x2, float y2)
+{
+	//Sort Y;
+	float sortY[3]
+	{
+		y0, y1, y2
+	};
+	float sortX[3]
+	{
+		x0, x1, x2
+	};
+
+	for(i32 i = 0; i < 3 - 1; i++)
+	{
+		for (i32 j = 0; j < 3 - i - 1; j++)
+		{
+			if (sortY[j] < sortY[j + 1])
+			{
+				float temp = sortY[j];
+				sortY[j] = sortY[j + 1];
+				sortY[j + 1] = temp;
+
+				temp = sortX[j];
+				sortX[j] = sortX[j + 1];
+				sortX[j + 1] = temp;
+			}
+		}
+	}
+	//TopFlatTri
+	if (sortY[0] == sortY[1])//우선은 Epsilon 생각 안하기로
+	{
+		DrawFlatTri(sortX[2], sortY[2], sortX[0], sortX[1], sortY[0]);
+	}
+	else if (sortY[1] == sortY[2])
+	{
+		DrawFlatTri(sortX[0], sortY[0], sortX[1], sortX[2], sortY[1]);
+	}
+	else
+	{
+		float newX = sortX[0] + (sortY[1] - sortY[0]) / (sortY[2] - sortY[0]) * (sortX[2] - sortX[0]);//닮음 이용
+		float newY = sortY[1];
+
+		DrawFlatTri(sortX[0], sortY[0], sortX[1], newX, newY);
+		DrawFlatTri(sortX[2], sortY[2], sortX[1], newX, newY);
+	}
+}
+
+void SoftRenderer::DrawFlatTri(float centerX, float centerY, float x1, float x2, float flatY)
+{
+	int fillDir = centerY > flatY ? -1 : 1;
+
+	float dy = flatY - centerY;
+	float leftdX = (x1 - centerX) / dy * fillDir;//BottomFlat일 경우 기울기의 부호를 뒤집어 줘야함
+	float rightdX = (x2 - centerX) / dy * fillDir;
+
+	if (leftdX > rightdX)
+	{
+		float temp = rightdX;
+		rightdX = leftdX;
+		leftdX = temp;
+	}
+	dy = abs(dy);
+	float curLeftX = centerX;
+	float curRightX = centerX;
+
+	int curY = (int)round(centerY);
+
+	for (int i = 0; i <= dy; i++)
+	{
+		int leftPointX = (int)ceil(curLeftX);
+		int rightPointX = (int)curRightX;
+		for (int j = leftPointX; j < rightPointX; j++)
+		{
+			DrawPixel(j, curY);
+		}
+		curY += fillDir;
+		curLeftX += leftdX;
+		curRightX += rightdX;
+	}
+}
+
+void SoftRenderer::DrawLine(float x0, float y0, float x1, float y1)
+{
+	float currentX, currentY, dx, dy, step;
+	int i = 0;
+	dx = x1 - x0;
+	dy = y1 - y0;
+
+	if (abs(dx) >= abs(dy))
+	{
+		step = abs(dx);
+	}
+	else
+	{
+		step = abs(dy);
+	}
+
+	dx /= step;
+	dy /= step;
+	
+	currentX = x0;
+	currentY = y0;
+	while (i <= step)
+	{
+		int xPoint = (int)round(currentX);
+		int yPoint = (int)round(currentY);
+		DrawPixel(xPoint, yPoint);
+
+		currentX += dx;
+		currentY += dy;
+		i++;
+	}
+}
+
 void SoftRenderer::UpdateFrame()
 {
 	// Buffer Clear
@@ -44,19 +156,18 @@ void SoftRenderer::UpdateFrame()
 	mGDIHelper->Clear();
 
 	// Draw vertical line
-	mGDIHelper->SetColor(255, 0, 0);
-	for (i32 line = -100; line <= 100; ++line)
-	{
-		DrawPixel(0, line);
-	}
+	mGDIHelper->SetColor(255, 255, 255);
+	DrawTri(0.0f,0.0f, 200.0f,100.0f, 200.0f, -300.0f);
 
-	// Draw horizontal line
-	for (i32 line = -100; line <= 100; ++line)
-	{
-		DrawPixel(line, 0);
-	}
 	
+
+
+
+
+
 	// Buffer Swap 
 	mGDIHelper->BufferSwap();
 }
+
+
 
--- Source/Renderer/SoftRenderer.h
+++ Source/Renderer/SoftRenderer.h
@@ -13,9 +13,10 @@ public:
 	void UpdateFrame(void);
 	bool IsInRange(i32 x, i32 y);
 	void DrawPixel(i32 x, i32 y);
-
+	void DrawTri(float x0, float y0, float x1, float y1, float x2, float y2);
+	void DrawLine(float x0, float y0, float x1, float y1);
 private:
 
 	class GDIHelper* mGDIHelper;
-
+	void DrawFlatTri(float centerX, float centerY, float x1, float x2, float flatY);
 };
